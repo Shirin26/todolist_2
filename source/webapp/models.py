62f277fb67605badc7d0fc3d838d434a2f9f428f
Shirin_Musaeva <shirinmusaeva26@gmail.com>
2022-11-18 19:28:17 +0600

    создала поле description, создала и применила миграции

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -6,6 +6,7 @@ class Exercise(models.Model):
     title = models.CharField(max_length=100, verbose_name='Описание')
     status = models.CharField(max_length=50, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0], verbose_name='Статус')
     todo_date = models.DateField(null=True, blank=True, verbose_name='Дата выполнения')
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание')
 
     def __str__(self):
         return f'{self.id}. {self.title}'
