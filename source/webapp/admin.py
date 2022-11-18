53ce750782b948249574a4d9c183609285531ca8
Shirin_Musaeva <shirinmusaeva26@gmail.com>
2022-11-18 15:56:44 +0600

    создали модель Exercise, миграции, зарегистрировали в админке

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from webapp.models import Exercise
 # Register your models here.
+
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'status', 'todo_date']
+    list_filter = ['status']
+    search_fields = ['title']
+    exclude = []
+
+admin.site.register(Exercise, ExerciseAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
+
+class Exercise(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Описание')
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0], verbose_name='Статус')
+    todo_date = models.DateField(null=True, blank=True, verbose_name='Дата выполнения')
+
+    def __str__(self):
+        return f'{self.id}. {self.title}'
