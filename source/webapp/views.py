e549238a1ad3de8eb519d71d0292c59880f665d1
Shirin_Musaeva <shirinmusaeva26@gmail.com>
2022-11-18 16:36:33 +0600

    создали вывод списка задач

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from webapp.models import Exercise
 
 # Create your views here.
+def index_view(request):
+    exercises = Exercise.objects.all()
+    return render(request, 'index.html', {'exercises': exercises})
