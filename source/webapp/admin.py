87015d4e8ce78a224402af45a0dcb0b7f0d665ab
Shirin_Musaeva <shirinmusaeva26@gmail.com>
2022-11-26 03:30:31 +0600

    Создала модели Status и Type, зарегистрировала в админке

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from webapp.models import Exercise
+from webapp.models import Exercise, Status, Type
 # Register your models here.
 
 class ExerciseAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'status', 'todo_date']
+    list_display = ['id', 'title', 'description', 'status', 'type']
     list_filter = ['status']
     search_fields = ['title']
     exclude = []
 
 admin.site.register(Exercise, ExerciseAdmin)
+admin.site.register(Status)
+admin.site.register(Type)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,12 +1,27 @@
 from django.db import models
 
-STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
+class Type(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Тип')
+
+    def __str__(self):
+        return self.name
+
+class Status(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Статус')
+
+    def __str__(self):
+        return self.name
+
 
 class Exercise(models.Model):
     title = models.CharField(max_length=100, verbose_name='Описание')
-    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0], verbose_name='Статус')
-    todo_date = models.DateField(null=True, blank=True, verbose_name='Дата выполнения')
     description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Описание')
+    status = models.ForeignKey('webapp.Status', related_name='exercises', on_delete=models.PROTECT, verbose_name='Статус')
+    type = models.ForeignKey('webapp.Type', related_name='exercises', on_delete=models.PROTECT, verbose_name='Тип')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время редактирования')
+
 
     def __str__(self):
         return f'{self.id}. {self.title}'
+
