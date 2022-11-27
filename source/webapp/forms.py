b903f023ddc49a1064bed5466e6ac1ad86bca2fd
Shirin_Musaeva <shirinmusaeva26@gmail.com>
2022-11-28 03:01:17 +0600

    types добавила в forms.py

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -5,5 +5,6 @@ class ExerciseForm(forms.Form):
     title = forms.CharField(max_length=100, required=True, label='Title')
     description = forms.CharField(max_length=3000, required=False, label='Description', widget=widgets.Textarea)
     status = forms.ModelChoiceField(queryset=Status.objects.all(), required=True, label='Status')
-    type = forms.ModelChoiceField(queryset=Type.objects.all(), required=True, label='Type')
-
+    types = forms.ModelMultipleChoiceField(
+        queryset=Type.objects.all(),
+        required=False, label='Типы')
